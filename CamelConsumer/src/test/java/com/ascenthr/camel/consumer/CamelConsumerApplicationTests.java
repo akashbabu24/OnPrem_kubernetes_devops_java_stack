3df78b74112e6c5d3251c39321297eee54550853
dsuresh <dsuresh@newtglobal.com>
2020-07-13 22:10:07 +0530

    Corrected package and class name

--- CamelConsumer/src/test/java/com/ascenthr/camel/consumer/CamelConsumerApplicationTests.java
+++ CamelConsumer/src/test/java/com/ascenthr/camel/consumer/CamelConsumerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ascenthr.camel.producer;
+package com.ascenthr.camel.consumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CamelProducerApplicationTests {
+public class CamelConsumerApplicationTests {
 
 	@Test
 	public void contextLoads() {
